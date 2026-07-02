8c66f44b4333ee464d22b45e36284f0883bcccf0
Luke Nguyen Khanh <lukenguyenkhanh@gmail.com>
2026-07-01 21:25:24 -0700

    Use collection

--- migrate.js
+++ migrate.js
@@ -4,11 +4,11 @@ for (const id in SongList) {
     const song = SongList[id];
 
     let frontMatter = '---\n';
-    frontMatter += `layout: song\n`;
+    // frontMatter += `layout: song\n`;
     for (const key in song) {
         frontMatter += `${key}: ${song[key]}\n`;
     }
     frontMatter += '---\n';
 
-    fs.writeFileSync('_song/' + id + '.html', frontMatter);
+    fs.writeFileSync('_songs/' + id + '.html', frontMatter);
 }
